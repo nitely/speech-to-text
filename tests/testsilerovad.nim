9006071899eda7a09317ce87c00608172745b462
nitely <ecastroborsani@gmail.com>
2024-12-02 22:53:09 -0300

    vad

--- tests/testsilerovad.nim
+++ tests/testsilerovad.nim
@@ -1,4 +1,4 @@
-import std/strbasics
+import std/streams
 import ../src/silerovad
 
 template test(desc: string, body: untyped): untyped =
@@ -8,17 +8,28 @@ template test(desc: string, body: untyped): untyped =
 
 proc readWav(s: string): seq[float32] =
   ## Assumes 16kHz samplerate
-  const headerSize = 44
-  var t = readFile(s)
-  doAssert t.len >= headerSize
-  setSlice(t, headerSize .. t.len-1)
-  let numSamples = t.len div 2
-  result = newSeq[float32](numSamples)
-  for i in 0 .. numSamples-1:
-    var sample = 0'u16
-    sample += t[i*2].uint16
-    sample += t[i*2+1].uint16 shl 8
-    result[i] = float32(sample) / float32(high(int16))
+  var fs = newFileStream(s, fmRead)
+  defer: close(fs)
+  var buffer = ""
+  var chunkSize: uint32
+  fs.readStr(4, buffer)
+  doAssert buffer == "RIFF"
+  fs.readStr(4, buffer)
+  fs.readStr(4, buffer)
+  doAssert buffer == "WAVE"
+  while not fs.atEnd():
+    fs.readStr(4, buffer)
+    chunkSize = fs.readUint32()
+    if buffer == "data":
+      #echo "found data"
+      break
+    fs.setPosition(fs.getPosition() + int64(chunkSize))
+    if chunkSize mod 2 != 0:
+      discard fs.readChar()
+  doAssert not fs.atEnd()
+  result = newSeq[float32]()
+  while not fs.atEnd():
+    result.add float32(fs.readInt16()) / float32(int16.high)
 
 test "Test jfk.wav":
   let cfg = newDetectorConfig(
@@ -33,14 +44,9 @@ test "Test jfk.wav":
   let samples = readWav("./samples/jfk.wav")
   doAssert dtr.detect(samples) ==
     @[
-      Segment(startAt: 0.066, endAt: 0.254),
-      Segment(startAt: 0.514, endAt: 0.67),
-      Segment(startAt: 2.21, endAt: 3.326),
-      Segment(startAt: 3.586, endAt: 3.71),
-      Segment(startAt: 3.81, endAt: 4.126),
-      Segment(startAt: 4.482, endAt: 5.566),
-      Segment(startAt: 5.73, endAt: 6.462),
-      Segment(startAt: 6.53, endAt: 8.35),
-      Segment(startAt: 8.61, endAt: 10.718),
-      Segment(startAt: 10.882, endAt: 0.0)
+      Segment(startAt: 0.29, endAt: 2.238),
+      Segment(startAt: 3.586, endAt: 3.774),
+      Segment(startAt: 4.002, endAt: 4.382),
+      Segment(startAt: 5.378, endAt: 7.678),
+      Segment(startAt: 8.162, endAt: 10.654)
     ]
