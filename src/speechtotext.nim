35094d1f08fac070c34be499bf222f0f5609e314
nitely <ecastroborsani@gmail.com>
2024-12-04 17:42:21 -0300

    wip

--- src/speechtotext.nim
+++ src/speechtotext.nim
@@ -17,39 +17,54 @@ proc cb_log_disable(level: enum_ggml_log_level, message: cstring, user_data: poi
 
 whisper_log_set(cb_log_disable, nil)
 
-proc toText*(pcm: seq[float32]): string =
+type
+  SpeechToText* = object
+    modelPath: string
+    ctx: ptr struct_whisper_context
+
+proc `=sink`*(dest: var SpeechToText; src: SpeechToText) {.error.}
+proc `=dup`*(src: SpeechToText): SpeechToText {.error.}
+proc `=copy`*(dest: var SpeechToText; src: SpeechToText) {.error.}
+proc `=wasMoved`*(dest: var SpeechToText) {.error.}
+
+proc `=destroy`*(stt: SpeechToText) =
+  if stt.ctx != nil:
+    whisper_free(stt.ctx)
+  `=destroy`(stt.modelPath)
+
+proc initSpeechToText*(modelPath: string, useGpu = true): SpeechToText =
+  var cparams = whisper_context_default_params()
+  cparams.use_gpu = useGpu
+  cparams.flash_attn = false
+  result = SpeechToText(modelPath: modelPath)
+  result.ctx = whisper_init_from_file_with_params(result.modelPath.cstring, cparams)
+
+proc toText*(stt: SpeechToText, pcm: seq[float32], prompt = ""): string =
   if pcm.len == 0:
     return ""
   const extraPad = 1000
   var pcm = pcm
   if pcm.len < 16000+extraPad:
     pcm.setLen 16000+extraPad
-  let model = "./src/models/ggml-base.en-q5_0.bin"
-  #let model = "./src/models/ggml-large-v3-turbo-q5_0.bin"
-  #let model = "./src/models/ggml-model-whisper-small-q5_1.bin"
-  var cparams = whisper_context_default_params()
-  cparams.use_gpu = true
-  cparams.flash_attn = false
-  var ctx = whisper_init_from_file_with_params(model.cstring, cparams)
-  defer: whisper_free(ctx)
   var wparams = whisper_full_default_params(WHISPER_SAMPLING_GREEDY)
   wparams.print_progress = false
   wparams.print_special = false
   wparams.print_realtime = false
   wparams.print_timestamps = false
-  wparams.translate = false
   wparams.single_segment = false
+  wparams.translate = true
   wparams.max_tokens = 0
-  wparams.language = "en"
-  wparams.n_threads = 1
+  wparams.language = "ja"
+  wparams.n_threads = 2
   wparams.audio_ctx = 0
-  wparams.prompt_tokens = nil
-  if whisper_full(ctx, wparams, addr pcm[0], pcm.len.cint) != 0:
+  if prompt.len > 0:
+    wparams.initial_prompt = prompt.cstring
+  if whisper_full(stt.ctx, wparams, addr pcm[0], pcm.len.cint) != 0:
     echo "Error: failed to process audio"
     return ""
-  let n_segments = whisper_full_n_segments(ctx)
+  let n_segments = whisper_full_n_segments(stt.ctx)
   result = ""
   for i in 0 .. n_segments-1:
-    result.add whisper_full_get_segment_text(ctx, i)
+    result.add whisper_full_get_segment_text(stt.ctx, i)
     if i < n_segments-1:
       result.add " "
--- tests/testall.nim
+++ tests/testall.nim
@@ -16,11 +16,12 @@ test "Test jfk.wav":
   var dtr = newDetector(cfg)
   let samples = readWav("./samples/jfk.wav")
   let segments = dtr.detect(samples)
+  let stt = initSpeechToText("./src/models/ggml-base.en-q5_0.bin")
   var textSegments = newSeq[string]()
   for segment in segments:
     let startIndex = toInt(segment.startAt * 16000'f64)
     let endIndex = toInt(segment.endAt * 16000'f64)
-    textSegments.add samples[startIndex .. endIndex-1].toText
+    textSegments.add stt.toText(samples[startIndex .. endIndex-1])
   doAssert textSegments == @[
     " And so, my fellow Americans!",
     " Ask not!",
@@ -29,28 +30,31 @@ test "Test jfk.wav":
   ], $textSegments
 
 #test "Test jfk.wav":
-#  # the "ask not" is not found with the default settings
-#  let cfg = newDetectorConfig(
-#    modelPath = "./src/models/silero_vad.onnx",
-#    sampleRate = 16000,
-#    threshold = 0.5,
-#    minSilenceDurationMs = 300,
-#    speechPadMs = 250,
-#    logLevel = OrtLoggingLevel.ORT_LOGGING_LEVEL_WARNING
-#  )
-#  var dtr = newDetector(cfg)
-#  let samples = readWav("./samples/jfk3.wav")
-#  let segments = dtr.detect(samples)
-#  var textSegments = newSeq[string]()
-#  for segment in segments:
-#    let startIndex = toInt(segment.startAt * 16000'f64)
-#    let endIndex = toInt(segment.endAt * 16000'f64)
-#    echo samples[startIndex .. endIndex-1].toText
-#
-#  #doAssert textSegments == @[
-#  #  " And so, my fellow Americans!",
-#  #  " Ask not!",
-#  #  " What your country can do for you!",
-#  #  " Ask what you can do for your country."
-#  #], $textSegments
-
+when false:
+  # the "ask not" is not found with the default settings
+  let cfg = newDetectorConfig(
+    modelPath = "./src/models/silero_vad.onnx",
+    sampleRate = 16000,
+    threshold = 0.5,
+    minSilenceDurationMs = 100,
+    speechPadMs = 30,
+    logLevel = OrtLoggingLevel.ORT_LOGGING_LEVEL_WARNING
+  )
+  var dtr = newDetector(cfg)
+  let samples = readWav("./samples/testja.wav")
+  let segments = dtr.detect(samples)
+  let stt = initSpeechToText("./src/models/ggml-medium-q5_0.bin")
+  var prompt = ""
+  var textSegments = newSeq[string]()
+  for segment in segments:
+    let startIndex = toInt(segment.startAt * 16000'f64)
+    let endIndex = toInt(segment.endAt * 16000'f64)
+    let text = stt.toText(samples[startIndex .. endIndex-1])
+    #prompt.add text
+    echo text
+  #doAssert textSegments == @[
+  #  " And so, my fellow Americans!",
+  #  " Ask not!",
+  #  " What your country can do for you!",
+  #  " Ask what you can do for your country."
+  #], $textSegments
